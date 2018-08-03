f1c99fa78313d240e3b6b719fa1b420b0c74ec0d
Unknown <by.darklancer@gmail.com>
2018-08-03 14:00:55 -0400

    feat: usando los servicios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,76 +1,68 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Tarea, EstadoTarea } from './tarea';
 import { TareaService } from './tarea.service';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+
 })
 
-export class AppComponent implements OnInit {
-  title = 'Todo Listo!';
-  estadoTareas = EstadoTarea;
+export class AppComponent implements OnInit{
+
+  title = 'Todo Listo';
+  estadoTareas : EstadoTarea;
   tareaSeleccionada: Tarea;
-  tareas: any;
+  tareas: Array<Tarea>;
   newTarea: Tarea;
   estadosTareas: any;
-  domain: string = 'http://127.0.0.1:8000';
-
-  constructor(public tareaService: TareaService, private httpClient: HttpClient) {
-    this.tareas = [];
+  
+  constructor(public tareaService: TareaService){
+    this.tareas =[];
     this.newTarea = new Tarea(null, null, null, null);
   }
+ 
+  ngOnInit(){
+    this.tareaService.getTareas()
+    .subscribe((ts: Array<Tarea>) => { this.tareas = ts;} 
+    );
 
-  ngOnInit() {
-    // this.tareaService.getTareas()
-    //     .subscribe((ts: Array<Tarea>) => {
-    //       this.tareas = ts;
-    //     });
-    this.getTareas();
     this.getEstados();
   }
 
-  getTareas() {
-    this.httpClient.get(`${this.domain}/tareas/`).subscribe(data => {
-      this.tareas = data;
-    });
+  actualizarTarea(t: Tarea) {
   }
 
+  seleccionarTarea(t: Tarea){
+    this.tareaSeleccionada= t;
+  }
+  
+  crearTarea(){
+    console.log(this.newTarea);
+    this.tareaService.crearTarea(this.newTarea).subscribe(
+      response => { ts => {
+        this.tareaService.push(ts);
+      }},
+      error => console.log('error', error)
+    );
+  }
+  
   getEstados() {
-    this.httpClient.get(`${this.domain}/estados/`).subscribe(data => {
+    this.tareaService.getEstados().subscribe(data => {
       this.estadosTareas = data;
     });
-  }
 
-  actualizarTarea(t: Tarea) {
-    console.log(`La tarea ${t} fue actualizada!`);
-  }
-
-  seleccionarTarea(t: Tarea) {
-    this.tareaSeleccionada = t;
-  }
-
-  crearTarea() {
-    this.httpClient.post(`${this.domain}/tareas/`, this.newTarea).subscribe(data => {
-    });
-    location.reload();
   }
 
   estado2str(e: EstadoTarea) {
     switch (e) {
-      case EstadoTarea.Creada: return 'Creada';
+      case EstadoTarea.Creada:    return 'Creada';
       case EstadoTarea.EnProceso: return 'En Proceso';
       case EstadoTarea.Terminada: return 'Terminada';
     }
   }
-
 }
-
-
-
-
 
 
 
--- src/app/tarea.service.ts
+++ src/app/tarea.service.ts
@@ -1,30 +1,44 @@
 import { Injectable } from '@angular/core';
 import { Tarea } from './tarea';
 import { Observable, of, empty } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TareaService {
 
-  constructor() { }
+  domainTareas = "http://localhost:8000/tareas/";
+  domainEstados = "http://localhost:8000/estados/";
+  push;
 
-  tareas: Array<Tarea> = [
-    new Tarea(1, 'Comprar leche', 'Y pañales para el bebe')
-    , new Tarea(2, 'Hacer Taller Angular', 'Falta empezar...')
-    , new Tarea(3, 'Preparar papers', 'Por fin')
-  ];
+  constructor(private httpClient: HttpClient) { }
 
-crearTarea(t: Tarea): Observable<any> {
-    // Obtener maximo id en this.tareas e incrementar en 1 para el nuevo id
-    const newId = Math.max.apply(null, this.tareas.map(x => x.id)) + 1;
-    // Insertar en el 'backend' la nueva tarea con el id generado y sus atributos
-    this.tareas.push(new Tarea(newId, t.titulo, t.descripcion));
-    // Se retorna un observable vacío solamente para seguir usando observables
-    return empty();
+  // tareas: Array<Tarea> = [
+  //   new Tarea(1, 'Comprar leche', 'Y pañales para el bebe')
+  //   , new Tarea(2, 'Hacer Taller Angular', 'Falta empezar...')
+  //   , new Tarea(3, 'Preparar papers', 'Por fin')
+  // ];
+
+  getTareas(): Observable<any>{
+    return this.httpClient.get<Array<any>>(this.domainTareas);
   }
 
-  getTareas(): Observable<any> {
-    return of(this.tareas);
+  getEstados(): Observable<any>{
+    return this.httpClient.get<Array<any>>(this.domainEstados);
   }
+  crearTarea(t: Tarea): Observable<any> {
+    location.reload();
+    return this.httpClient.post<Tarea>(this.domainTareas, t);
+  }
+
+// crearTarea(t: Tarea): Observable<any> {
+//     // Obtener maximo id en this.tareas e incrementar en 1 para el nuevo id
+//     const newId = Math.max.apply(null, this.tareas.map(x => x.id)) + 1;
+//     // Insertar en el 'backend' la nueva tarea con el id generado y sus atributos
+//     this.tareas.push(new Tarea(newId, t.titulo, t.descripcion));
+//     // Se retorna un observable vacío solamente para seguir usando observables
+//     return empty();
+//   }
+
 }
